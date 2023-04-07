f2de8be0de60aa5ddc501b88a895dda94f8f8508
hussain2023 <iamhussainali2@gmail.com>
2023-04-07 14:46:39 +0530

    added blog design

--- src/Pages/Blog.js
+++ src/Pages/Blog.js
@@ -1,9 +1,164 @@
 import React from "react";
+import blogImg from "../assets/Rectangle 46.png";
+import rightArrow from "../assets/next.png";
+import leftArrow from "../assets/previous.png";
+import img5 from "../assets/image 5.png";
+import img6 from "../assets/Naruto-To-Boruto-Shinobi-Striker-Naruto-Uzumaki-Last-Battle-1 1.png";
+import img7 from "../assets/image 7.png";
 
 const Blog = () => {
   return (
     <div>
-      <h1 className="text-7xl">This is Blog Page</h1>
+      <div>
+        <img
+          src={blogImg}
+          className=" w-screen px-4 md:px-20 py-4 relative"
+          alt=""
+        />
+        <h1 className=" uppercase text-2xl md:text-5xl font-bold absolute top-[120px] md:top-[250px] left-[50px] md:left-[500px] text-white">
+          attack on the titans
+        </h1>
+      </div>
+      <div className="md:px-20 px-4">
+        <h3 className=" text-left uppercase text-2xl">Chapter 1</h3>
+        <hr />
+        <p className=" text-justify pt-3 pb-8">
+          Aliquam porta nisl dolor, molestie pellentesque elit molestie in.
+          Morbi metus neque, elementum ullamcorper hendrerit eget, tincidunt et
+          nisi. Sed magna nunc, consequat vel aliquam vitae, porta ac mi.
+          Integer commodo sapien lacus, nec interdum nisi vehicula aliquam.
+          Aliquam enim lorem, laoreet ut egestas quis, rutrum sed lectus. Duis
+          et purus in dolor bibendum egestas vel vitae urna. Vestibulum dictum
+          semper laoreet. Nulla augue tellus, aliquam mollis quam eget, maximus
+          iaculis sem. Praesent semper ex tortor, in rhoncus arcu sollicitudin
+          ut. Donec magna lorem, efficitur quis fringilla sed, imperdiet sed
+          risus. Nam accumsan, elit sit amet pretium commodo, turpis augue
+          molestie eros, quis maximus diam ex eget libero. Aliquam erat
+          volutpat. Integer interdum arcu nisi. Cras venenatis lorem in molestie
+          pulvinar. Donec a tortor et magna suscipit consectetur. Sed mi lacus,
+          vehicula id ex nec, vulputate pretium urna. Aenean accumsan facilisis
+          ornare. Aenean porttitor augue at erat aliquam, quis mattis nunc
+          egestas. Morbi malesuada lorem tortor, sed aliquam ante laoreet et.
+          Nulla facilisi. Nulla nisl velit, blandit vel sagittis et, venenatis
+          et leo. Mauris nec metus pretium, hendrerit lectus in, iaculis diam.
+          Fusce massa odio, lacinia id leo at, eleifend egestas nibh. Etiam
+          placerat magna sit amet scelerisque pulvinar. Quisque tempor ex nulla.
+          Nullam non aliquam mauris, nec semper velit. Duis aliquam fermentum
+          sem quis fermentum. Nunc at nunc vitae sem tempor vestibulum. Sed
+          commodo a tortor placerat lacinia. Mauris placerat tempor nibh,
+          lobortis imperdiet mauris egestas nec. Quisque tincidunt ex non lacus
+          finibus euismod. Praesent congue turpis a pulvinar varius. Etiam
+          tempor sapien in viverra tincidunt. Donec vel lacus ultricies, rhoncus
+          odio sit amet, gravida magna. Curabitur posuere lacus id diam porta
+          pulvinar. Maecenas lacus mauris, laoreet quis hendrerit eget, lacinia
+          et elit. Donec venenatis condimentum quam, at feugiat massa mollis
+          ullamcorper. Aliquam accumsan mi vitae leo tempor venenatis. Nulla
+          facilisi. Vivamus vestibulum, turpis eu varius venenatis, odio felis
+          tempor nulla, ut placerat quam nisl in tellus. Cras rutrum fermentum
+          tempor. Aliquam volutpat imperdiet sapien, ac volutpat lectus rhoncus
+          in. Cras tortor nulla, ornare ac urna a, volutpat pellentesque tellus.
+          Fusce volutpat sapien porttitor, sodales arcu quis, malesuada augue.
+          Pellentesque eu fringilla enim, a scelerisque ligula. Sed vel mollis
+          diam, id ornare mauris. Quisque non hendrerit nisl, vel finibus
+          tellus. Nulla condimentum metus mattis, aliquet magna sit amet, dictum
+          nisl. Integer vehicula fermentum lobortis. Integer vel placerat sem.
+          Donec viverra vulputate ligula, ut imperdiet felis condimentum eu.
+          Duis at sollicitudin odio, ac finibus nibh. Etiam eu libero molestie,
+          iaculis nisi vel, varius nibh. Donec pharetra augue quis nulla
+          efficitur, at ultricies risus lacinia. Sed fermentum arcu a viverra
+          elementum. Duis viverra elit ac ligula eleifend egestas. Cras mollis
+          ut libero sed euismod. Sed at rhoncus ipsum, et malesuada nulla. Fusce
+          sit amet mauris sit amet leo gravida pulvinar. Etiam sapien magna,
+          tempor ut augue sit amet, pellentesque convallis tellus. Suspendisse
+          potenti. Vivamus quis aliquam urna. Class aptent taciti sociosqu ad
+          litora torquent per conubia nostra, per inceptos himenaeos. Nulla
+          varius neque et sem convallis, eu sollicitudin velit luctus. Aenean
+          mollis diam commodo massa rhoncus porta. In dignissim imperdiet
+          varius. In ut massa ex. Quisque tincidunt ultricies nisi, sit amet
+          lobortis velit imperdiet vel. Suspendisse sed convallis mauris, non
+          aliquam lorem. Duis augue justo, dictum ut lacinia eget, dignissim
+          vitae urna. Ut bibendum elit vel viverra volutpat. Aliquam tempus nisl
+          tempor nunc consequat laoreet. Integer ut mattis nisi. Proin
+          tincidunt, risus sit amet eleifend rhoncus, lectus orci dapibus nibh,
+          sit amet vestibulum est justo ut ligula. Orci varius natoque penatibus
+          et magnis dis parturient montes, nascetur ridiculus mus. Ut sed
+          eleifend urna. Curabitur eleifend cursus nulla, in egestas eros
+          rhoncus quis. Quisque placerat facilisis lacinia. Cras venenatis
+          hendrerit cursus. Morbi neque nisi, ultrices eget rhoncus sit amet,
+          ornare at dolor. Vivamus vitae libero eu augue semper porta sit amet
+          eleifend leo. Sed quis finibus arcu, eget dapibus lacus. Nulla egestas
+          malesuada magna, quis rutrum risus imperdiet vel. Donec porttitor
+          fringilla quam, eget elementum odio sodales et. Proin tincidunt nunc
+          ac dolor dapibus, a consequat massa volutpat. Morbi augue arcu,
+          sodales vel dictum ac, congue placerat purus. Aliquam tincidunt
+          pharetra neque non hendrerit. Integer eget tincidunt urna, sit amet
+          feugiat orci. Praesent dictum fringilla mi, mollis consequat lorem
+          vulputate sit amet. Curabitur tempus purus efficitur, tempus orci
+          quis, aliquet ligula. Morbi condimentum vel ligula et tincidunt.
+          Nullam blandit odio augue, quis cursus mi cursus ac. Nullam auctor
+          sagittis semper. Proin vitae erat in nunc hendrerit ultrices
+          scelerisque sit amet mauris. Praesent ultricies odio quis urna viverra
+          commodo. Ut eleifend laoreet ultricies. Mauris ac tempor elit.
+          Curabitur congue semper lacus, facilisis
+        </p>
+        <hr />
+        <div className="flex justify-end pt-4">
+          <div className=" flex gap-3">
+            <img className=" w-8" src={rightArrow} alt="" />
+            <h3 className=" text-right uppercase text-2xl">chapter 2</h3>
+          </div>
+        </div>
+
+        <div className="flex justify-between pt-24 pb-1">
+          <h4 className=" text-left text-xl">Short Reads</h4>
+          <div className="flex gap-6">
+            <img className="w-8" src={leftArrow} alt="" />
+            <img className="w-8" src={rightArrow} alt="" />
+          </div>
+        </div>
+        <hr />
+        <div className="md:flex flex-wrap md:justify-between pt-10">
+          <div className="flex">
+            <div>
+              <img src={img5} alt="" />
+            </div>
+            <div className="">
+              <p className="text-left text-lg font-medium pl-3">
+                Akame Ga kill: <br /> Season finale
+              </p>
+              <p className="pl-3">Lorem ipsum dolor sit amet,</p>
+              <p>consectetur adipiscing elit</p>
+            </div>
+          </div>
+          <div className="flex">
+            <div>
+              <img src={img6} alt="" />
+            </div>
+            <div className="">
+              <p className="text-left text-lg font-medium pl-3">
+                Naruto Uzumaki: <br /> Hidden Village
+              </p>
+              <p className="pl-3">Lorem ipsum dolor sit amet,</p>
+              <p>consectetur adipiscing elit</p>
+            </div>
+          </div>
+          <div className="flex">
+            <div>
+              <img src={img7} alt="" />
+            </div>
+            <div className="">
+              <p className="text-left text-lg font-medium pl-3">
+                Love juice Season <br /> Priemere
+              </p>
+              <p>
+                <span className=" pl-3">Lorem ipsum dolor sit amet,</span>
+                <br />
+                <span>consectetur adipiscing elit</span>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
