4ce37deb699e385ea72fba36965fe1b989b1ab34
hussain2023 <iamhussainali2@gmail.com>
2023-04-07 09:05:18 +0530

    navar design

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Navbar from "./Pages/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="background-image">
         <h1 className="text-white">Hi there</h1>
       </div>
---
+++ src/Pages/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className=" pr-20 flex justify-end py-5">
+      <p className=" text-sm mr-14">Blog</p>
+      <p className="rounded-md">Sign Up</p>
+      <p className=" ml-14">Login</p>
+    </div>
+  );
+};
+
+export default Navbar;
